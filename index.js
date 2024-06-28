136a320d68c63d75c54f81825eb7568725b6dc2e
Aron <aron.kipkorir@student.moringaschool.com>
2024-06-28 20:59:11 +0300

    Adds scope

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+
+window.customerName = "bob";
+
+function upperCaseCustomerName() {
+    return window.customerName = customerName.toUpperCase();
+}
+
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+
+function overwriteBestCustomer(newBestCustomer){
+    bestCustomer = newBestCustomer;
+}
+
+const leastFavoriteCustomer = 'john';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'jane';
+}
